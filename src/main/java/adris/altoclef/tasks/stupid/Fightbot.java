ab2e17031b1ea962ef248261a002d433a9b99070
JustaSqu1d <overenchanted.gaming@gmail.com>
2022-11-22 21:51:42 -0800

    Refactor FightBot

--- src/main/java/adris/altoclef/tasks/stupid/Fightbot.java
+++ src/main/java/adris/altoclef/tasks/stupid/Fightbot.java
@@ -180,7 +180,7 @@ public class Fightbot extends Task {
 
     @Override
     protected String toDebugString() {
-        return "Fightbot ";
+        return "FightBot ";
     }
 
     private boolean isReadyToPunk(AltoClef mod) {
