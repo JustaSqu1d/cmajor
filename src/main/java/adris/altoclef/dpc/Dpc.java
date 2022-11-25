39f06912c8d8d3fc4898cef42f1d7ebe08d6388b
JustaSqu1d <overenchanted.gaming@gmail.com>
2022-11-25 10:33:02 -0800

    fix: comment out JDA

--- src/main/java/adris/altoclef/dpc/Dpc.java
+++ src/main/java/adris/altoclef/dpc/Dpc.java
@@ -1,148 +1,152 @@
-package adris.altoclef.dpc;
-
-import adris.altoclef.AltoClef;
-import adris.altoclef.Debug;
-import adris.altoclef.eventbus.EventBus;
-import adris.altoclef.eventbus.events.TaskFinishedEvent;
-import adris.altoclef.ui.MessagePriority;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.InteractionHook;
-
-/**
- * The Dpc system lets authorized players send commands to the bot to execute.
- * <p>
- * This effectively makes the bot function as a servant, or Dpc.
- * <p>
- * Authorization is defined in "altoclef_Dpc_whitelist.txt" and "altoclef_Dpc_blacklist.txt"
- * and depends on the "useDpcWhitelist" and "useDpcBlacklist" settings in "altoclef_settings.json"
- */
-public class Dpc extends ListenerAdapter {
-
-    private static final String Dpc_MESSAGE_START = "` ";
-    private final AltoClef _mod;
-    private final UserAuth _userAuth;
-    private String _currentUser = null;
-    // Utility variables for command logic
-    private boolean _commandInstantRan = false;
-    private boolean _commandFinished = false;
-
-    public Dpc(AltoClef mod) {
-        _mod = mod;
-        _userAuth = new UserAuth(mod);
-
-        // Revoke our current user whenever a task finishes.
-        EventBus.subscribe(TaskFinishedEvent.class, evt -> {
-            if (_currentUser != null) {
-                _currentUser = null;
-            }
-        });
-    }
-
-    @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        // Only accept commands from guilds
-        if (event.getGuild() == null)
-            return;
-        if ("run".equals(event.getName())) {
-            String message = event.getOption("content").getAsString();
-            _mod.getDpc().processCommand(event, message);
-        }
-    }
-
-    private void processCommand(SlashCommandInteractionEvent event, String message) {
-        event.deferReply(true).queue(); // Let the user know we received the command before doing anything else
-        InteractionHook hook = event.getHook(); // This is a special webhook that allows you to send messages without having permissions in the channel and also allows ephemeral messages
-        hook.setEphemeral(true); // All messages here will now be ephemeral implicitly
-
-        Member member = event.getMember();
-
-        boolean debug = DpcConfig.getInstance().whisperFormatDebug;
-
-        if (debug) {
-            Debug.logMessage("RECEIVED WHISPER: \"" + message + "\".");
-        }
-
-        if (_userAuth.isUserAuthorized(member)) {
-            executeWhisper(hook, member, message);
-        } else {
-            if (debug) {
-                Debug.logMessage("    Rejecting: User \"" + member + "\" is not authorized.");
-            }
-            if (DpcConfig.getInstance().sendAuthorizationResponse) {
-                hook.sendMessage("You don't have the required permissions.").queue();
-                return;
-            }
-        }
-    }
-
-
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
-    public boolean isUserAuthorized(Member username) {
-        return _userAuth.isUserAuthorized(username);
-    }
-
-    public void onLog(String message, MessagePriority priority) {
-        if (_currentUser != null) {
-            sendWhisper(message, priority);
-        }
-    }
-
-    public void onLogWarning(String message, MessagePriority priority) {
-        if (_currentUser != null) {
-            sendWhisper("[WARNING:] " + message, priority);
-        }
-    }
-
-    public void tick() {
-        // Nothing for now.
-    }
-
-    public String getCurrentUser() {
-        return _currentUser;
-    }
-
-    public boolean hasCurrentUser() {
-        return _currentUser != null;
-    }
-
-    private void executeWhisper(InteractionHook hook, Member member, String message) {
-        String prevUser = _currentUser;
-        _commandInstantRan = true;
-        _commandFinished = false;
-        _currentUser = member.getId();
-        hook.sendMessage("Command Executing: `" + message + "`").queue();
-        String prefix = DpcConfig.getInstance().requirePrefixMsg ? _mod.getModSettings().getCommandPrefix() : "";
-        AltoClef.getCommandExecutor().execute(prefix + message, () -> {
-            // On finish
-            hook.editOriginal("Command Finished: " + message).queue();
-            if (!_commandInstantRan) {
-                _currentUser = null;
-            }
-            _commandFinished = true;
-        }, e -> {
-            hook.editOriginal("TASK FAILED: " + e.getMessage()).queue();
-            e.printStackTrace();
-            _currentUser = null;
-            _commandInstantRan = false;
-        });
-        _commandInstantRan = false;
-        // Only set the current user if we're still running.
-        if (_commandFinished) {
-            _currentUser = prevUser;
-        }
-    }
-
-    private void sendWhisper(String message, MessagePriority priority) {
-        if (_currentUser != null) {
-            sendWhisper(_currentUser, message, priority);
-        } else {
-            Debug.logWarning("Failed to send Dpc message as there are no users present: " + message);
-        }
-    }
-
-    private void sendWhisper(String username, String message, MessagePriority priority) {
-        _mod.getMessageSender().enqueueWhisper(username, Dpc_MESSAGE_START + message, priority);
-    }
-}
+//package adris.altoclef.dpc;
+//
+//import adris.altoclef.AltoClef;
+//import adris.altoclef.Debug;
+//import adris.altoclef.eventbus.EventBus;
+//import adris.altoclef.eventbus.events.TaskFinishedEvent;
+//import adris.altoclef.ui.MessagePriority;
+//import net.dv8tion.jda.api.entities.Member;
+//import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+//import net.dv8tion.jda.api.hooks.ListenerAdapter;
+//import net.dv8tion.jda.api.interactions.InteractionHook;
+////import net.dv8tion.jda.api.entities.Member;
+////import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+////import net.dv8tion.jda.api.hooks.ListenerAdapter;
+////import net.dv8tion.jda.api.interactions.InteractionHook;
+//
+///**
+// * The Dpc system lets authorized players send commands to the bot to execute.
+// * <p>
+// * This effectively makes the bot function as a servant, or Dpc.
+// * <p>
+// * Authorization is defined in "altoclef_Dpc_whitelist.txt" and "altoclef_Dpc_blacklist.txt"
+// * and depends on the "useDpcWhitelist" and "useDpcBlacklist" settings in "altoclef_settings.json"
+// */
+//public class Dpc extends ListenerAdapter {
+//
+//    private static final String Dpc_MESSAGE_START = "` ";
+//    private final AltoClef _mod;
+//    private final UserAuth _userAuth;
+//    private String _currentUser = null;
+//    // Utility variables for command logic
+//    private boolean _commandInstantRan = false;
+//    private boolean _commandFinished = false;
+//
+//    public Dpc(AltoClef mod) {
+//        _mod = mod;
+//        _userAuth = new UserAuth(mod);
+//
+//        // Revoke our current user whenever a task finishes.
+//        EventBus.subscribe(TaskFinishedEvent.class, evt -> {
+//            if (_currentUser != null) {
+//                _currentUser = null;
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+//        // Only accept commands from guilds
+//        if (event.getGuild() == null)
+//            return;
+//        if ("run".equals(event.getName())) {
+//            String message = event.getOption("content").getAsString();
+//            _mod.getDpc().processCommand(event, message);
+//        }
+//    }
+//
+//    private void processCommand(SlashCommandInteractionEvent event, String message) {
+//        event.deferReply(true).queue(); // Let the user know we received the command before doing anything else
+//        InteractionHook hook = event.getHook(); // This is a special webhook that allows you to send messages without having permissions in the channel and also allows ephemeral messages
+//        hook.setEphemeral(true); // All messages here will now be ephemeral implicitly
+//
+//        Member member = event.getMember();
+//
+//        boolean debug = DpcConfig.getInstance().whisperFormatDebug;
+//
+//        if (debug) {
+//            Debug.logMessage("RECEIVED WHISPER: \"" + message + "\".");
+//        }
+//
+//        if (_userAuth.isUserAuthorized(member)) {
+//            executeWhisper(hook, member, message);
+//        } else {
+//            if (debug) {
+//                Debug.logMessage("    Rejecting: User \"" + member + "\" is not authorized.");
+//            }
+//            if (DpcConfig.getInstance().sendAuthorizationResponse) {
+//                hook.sendMessage("You don't have the required permissions.").queue();
+//                return;
+//            }
+//        }
+//    }
+//
+//
+//    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+//    public boolean isUserAuthorized(Member username) {
+//        return _userAuth.isUserAuthorized(username);
+//    }
+//
+//    public void onLog(String message, MessagePriority priority) {
+//        if (_currentUser != null) {
+//            sendWhisper(message, priority);
+//        }
+//    }
+//
+//    public void onLogWarning(String message, MessagePriority priority) {
+//        if (_currentUser != null) {
+//            sendWhisper("[WARNING:] " + message, priority);
+//        }
+//    }
+//
+//    public void tick() {
+//        // Nothing for now.
+//    }
+//
+//    public String getCurrentUser() {
+//        return _currentUser;
+//    }
+//
+//    public boolean hasCurrentUser() {
+//        return _currentUser != null;
+//    }
+//
+//    private void executeWhisper(InteractionHook hook, Member member, String message) {
+//        String prevUser = _currentUser;
+//        _commandInstantRan = true;
+//        _commandFinished = false;
+//        _currentUser = member.getId();
+//        hook.sendMessage("Command Executing: `" + message + "`").queue();
+//        String prefix = DpcConfig.getInstance().requirePrefixMsg ? _mod.getModSettings().getCommandPrefix() : "";
+//        AltoClef.getCommandExecutor().execute(prefix + message, () -> {
+//            // On finish
+//            hook.editOriginal("Command Finished: " + message).queue();
+//            if (!_commandInstantRan) {
+//                _currentUser = null;
+//            }
+//            _commandFinished = true;
+//        }, e -> {
+//            hook.editOriginal("TASK FAILED: " + e.getMessage()).queue();
+//            e.printStackTrace();
+//            _currentUser = null;
+//            _commandInstantRan = false;
+//        });
+//        _commandInstantRan = false;
+//        // Only set the current user if we're still running.
+//        if (_commandFinished) {
+//            _currentUser = prevUser;
+//        }
+//    }
+//
+//    private void sendWhisper(String message, MessagePriority priority) {
+//        if (_currentUser != null) {
+//            sendWhisper(_currentUser, message, priority);
+//        } else {
+//            Debug.logWarning("Failed to send Dpc message as there are no users present: " + message);
+//        }
+//    }
+//
+//    private void sendWhisper(String username, String message, MessagePriority priority) {
+//        _mod.getMessageSender().enqueueWhisper(username, Dpc_MESSAGE_START + message, priority);
+//    }
+//}
--- src/main/java/adris/altoclef/dpc/DpcConfig.java
+++ src/main/java/adris/altoclef/dpc/DpcConfig.java
@@ -1,53 +1,53 @@
-package adris.altoclef.dpc;
-
-import adris.altoclef.util.helpers.ConfigHelper;
-
-public class DpcConfig {
-
-    private static DpcConfig _instance = new DpcConfig();
-
-    static {
-        ConfigHelper.loadConfig("configs/discord.json", DpcConfig::new, DpcConfig.class, newConfig -> _instance = newConfig);
-    }
-
-    public boolean useDpc = false;
-
-    /**
-     * If true, will use blacklist for rejecting users from using your player as a Dpc
-     */
-    public boolean useDpcBlacklist = true;
-    /**
-     * If true, will use whitelist to only accept users from said whitelist.
-     */
-    public boolean useDpcWhitelist = true;
-    /**
-     * If set to true, will print information about whispers that are parsed and those
-     * that have failed parsing.
-     * <p>
-     * Enable this if you need help setting up the whisper format.
-     */
-    public boolean whisperFormatDebug = false;
-    /**
-     * Determines if failure messages should be sent to a non-authorized entity attempting to use Dpc
-     * <p>
-     * Disable this if you need to stay undercover.
-     */
-    public boolean sendAuthorizationResponse = true;
-    /**
-     * The response sent in a failed execution due to non-authorization
-     * {from}: the username of the player who triggered the failed authorization response
-     */
-    public String failedAuthorizationResposne = "Sorry {from} but you are not authorized!";
-    /**
-     * Use this to choose if the prefix should be required in messages
-     * <p>
-     * Disable this if you want to be able to send normal messages and not Dpc commands.
-     */
-    public boolean requirePrefixMsg = false;
-
-    public String botToken = "";
-
-    public static DpcConfig getInstance() {
-        return _instance;
-    }
-}
+//package adris.altoclef.dpc;
+//
+//import adris.altoclef.util.helpers.ConfigHelper;
+//
+//public class DpcConfig {
+//
+//    private static DpcConfig _instance = new DpcConfig();
+//
+//    static {
+//        ConfigHelper.loadConfig("configs/discord.json", DpcConfig::new, DpcConfig.class, newConfig -> _instance = newConfig);
+//    }
+//
+//    public boolean useDpc = false;
+//
+//    /**
+//     * If true, will use blacklist for rejecting users from using your player as a Dpc
+//     */
+//    public boolean useDpcBlacklist = true;
+//    /**
+//     * If true, will use whitelist to only accept users from said whitelist.
+//     */
+//    public boolean useDpcWhitelist = true;
+//    /**
+//     * If set to true, will print information about whispers that are parsed and those
+//     * that have failed parsing.
+//     * <p>
+//     * Enable this if you need help setting up the whisper format.
+//     */
+//    public boolean whisperFormatDebug = false;
+//    /**
+//     * Determines if failure messages should be sent to a non-authorized entity attempting to use Dpc
+//     * <p>
+//     * Disable this if you need to stay undercover.
+//     */
+//    public boolean sendAuthorizationResponse = true;
+//    /**
+//     * The response sent in a failed execution due to non-authorization
+//     * {from}: the username of the player who triggered the failed authorization response
+//     */
+//    public String failedAuthorizationResposne = "Sorry {from} but you are not authorized!";
+//    /**
+//     * Use this to choose if the prefix should be required in messages
+//     * <p>
+//     * Disable this if you want to be able to send normal messages and not Dpc commands.
+//     */
+//    public boolean requirePrefixMsg = false;
+//
+//    public String botToken = "";
+//
+//    public static DpcConfig getInstance() {
+//        return _instance;
+//    }
+//}
